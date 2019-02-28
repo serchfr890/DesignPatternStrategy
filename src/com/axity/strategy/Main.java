f799738dcd81439824287432e3521f172845665f
serchfr <serfr890@outlook.es>
2019-02-28 05:03:59 -0600

    Se suben los archivos

---
+++ src/com/axity/strategy/Main.java
@@ -0,0 +1,21 @@
+package com.axity.strategy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ShoppingCart cart = new ShoppingCart();
+
+        Item item1 = new Item("1234",10);
+        Item item2 = new Item("5678", 50);
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        //pay for Paypal
+        cart.pay(new PaypalStrategy("sergio_fr@outlook.es","myped"));
+
+        //pay by credit card
+        cart.pay(new CreditCardStrategy("Sergio F","1234567891434567","246","12/15"));
+    }
+}
---
+++ src/com/axity/strategy/PaymentStratgy.java
@@ -0,0 +1,5 @@
+package com.axity.strategy;
+
+public interface PaymentStratgy {
+    public void pay(int amount);
+}
